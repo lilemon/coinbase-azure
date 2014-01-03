d39dd24bb2b9d1609082d5611d5acfcadcb475a7
Mohamed Mansour <hello@mohamedmansour.com>
2014-01-03 03:26:14 -0800

    Update API to enable local cache

---
+++ routes/api.js
@@ -0,0 +1,30 @@
+﻿var request = require('request');
+
+exports.attach = function(app) {
+  var BASE_URL = 'https://coinbase.com/api/v1/',
+      EXCHANGE_RATE_ENDPOINT = BASE_URL + 'currencies/exchange_rates',
+      exchange_rate_map = {};
+
+  app.get('/api/exchange_rate', function(req, res) {
+    if (req.query.force) {
+      request(EXCHANGE_RATE_ENDPOINT, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          exchange_rate_map = JSON.parse(body);
+          res.send({
+            usd: exchange_rate_map['btc_to_usd']
+          });
+        }
+        else {
+          res.send({
+            error: error
+         });
+        }
+      })
+    }
+    else {
+      res.send({
+        usd:  exchange_rate_map['btc_to_usd']
+      });
+    }
+  });
+};
